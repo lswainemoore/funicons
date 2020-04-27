fe5b23d9f784d2dfd95487cc371356a3bb9415b6
lsm <lswainemoore@gmail.com>
2020-04-26 21:30:09 -0400

    next step of proof

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var toggleFav = () => {
+$(document).ready(function() {
   var link = $('#fav');
-  console.log('swap!');
-  if (link.attr('href').search('black') == -1) {
-    link.attr('href', solidSVG('black'));
-  } else {
-    link.attr('href', solidSVG('blue'));
-  }
-  setTimeout(toggleFav, 500);
-};
 
-var solidSVG = (colorStr) => {
-  return `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"><rect height="16" width="16" fill="${colorStr}" /></svg>`
-};
+  $('.split > div').each(function() {
+    $(this).mouseover(function() {
+      console.log(solidSVG($(this).attr('id')));
+      link.attr('href', solidSVG($(this).attr('id')));
+    });
+  });
 
-toggleFav();
+  var solidSVG = (colorStr) => {
+    return `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"><rect height="16" width="16" fill="${colorStr}" /></svg>`
+  };
+});
+
